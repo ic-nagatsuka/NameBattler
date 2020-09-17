d0308744494bcf3df25a97d7c483e3f0354d1e63
naga <CCCDDDBAABDC@naga>
2020-09-17 11:38:32 +0900

    トップ画面タイトルの大文字エラー修正

--- NameBattler/app/src/main/java/com/example/namebattler/TopScreen.java
+++ NameBattler/app/src/main/java/com/example/namebattler/TopScreen.java
@@ -9,6 +9,6 @@ public class TopScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_topScreen);
+        setContentView(R.layout.activity_top_screen);
     }
 }
