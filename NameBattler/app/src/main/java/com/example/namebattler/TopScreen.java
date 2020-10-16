5c1366ce884363e2b5641979eadd1cc3be78b425
naga <CCCDDDBAABDC@naga>
2020-10-16 10:55:22 +0900

    [2020/10/16]@0.2 キャラ一覧ボタンにキャラクター一覧画面への画面遷移を実装

--- NameBattler/app/src/main/java/com/example/namebattler/TopScreen.java
+++ NameBattler/app/src/main/java/com/example/namebattler/TopScreen.java
@@ -2,7 +2,10 @@ package com.example.namebattler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TopScreen extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class TopScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_screen);
+
+        Button characterDetails = findViewById(R.id.top_characterList);
+
+        characterDetails.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Intent intent = new Intent(
+                       TopScreen.this, CharacterList.class);
+
+               startActivity(intent);
+           }
+        });
+
+
+
     }
 }
