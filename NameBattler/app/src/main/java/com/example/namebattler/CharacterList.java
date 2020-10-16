6d111b4e2d573a93fcf0f29b551104ba4316292b
naga <CCCDDDBAABDC@naga>
2020-10-16 11:52:59 +0900

    [2020/10/16]@0.3 新しく作成するボタンにキャラクター作成完了画面への画面遷移を実装

--- NameBattler/app/src/main/java/com/example/namebattler/CharacterList.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterList.java
@@ -2,7 +2,17 @@ package com.example.namebattler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CharacterList extends AppCompatActivity {
 
@@ -10,5 +20,23 @@ public class CharacterList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_list);
+
+        Button charaMake = findViewById(R.id.character_list_MakeButton);
+        charaMake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CharacterList.this, CharacterMake.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
--- NameBattler/app/src/main/java/com/example/namebattler/CharacterMake.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterMake.java
@@ -9,6 +9,6 @@ public class CharacterMake extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_character_details);
+        setContentView(R.layout.activity_character_make);
     }
 }
