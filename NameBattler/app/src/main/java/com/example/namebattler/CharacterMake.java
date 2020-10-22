a09271c2798ef534467a0fed34d87dea8d88c1ce
naga <CCCDDDBAABDC@naga>
2020-10-22 13:33:53 +0900

    [2020/10/22]@0.5 ボタンにキャラクター作成完了画面への遷移を実装

--- NameBattler/app/src/main/java/com/example/namebattler/CharacterMake.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterMake.java
@@ -2,7 +2,11 @@ package com.example.namebattler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 
 public class CharacterMake extends AppCompatActivity {
@@ -11,5 +15,26 @@ public class CharacterMake extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_make);
+
+
+        findViewById(R.id.character_make_makeButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                RadioGroup radioGroup = findViewById(R.id.character_make_job_RadioGroup);
+                EditText editName = findViewById(R.id.character_make_editText_name);
+
+                if(!editName.getText().toString().equals("") && radioGroup.getCheckedRadioButtonId() != -1){
+                    Intent intent = new Intent(getApplication(), CharacterMakeSuccesScreen.class);
+                    startActivity(intent);
+                }
+
+
+            }
+
+
+        });
+
+
+
     }
 }
