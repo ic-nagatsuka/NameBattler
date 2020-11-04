63230803f015f711f225cb632eb645f6da9c0092
naga <CCCDDDBAABDC@naga>
2020-11-04 13:58:13 +0900

    [2020/11/4]@0.2 キャラクター詳細画面遷移時の送るデータ実装

--- NameBattler/app/src/main/java/com/example/namebattler/CharacterList.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterList.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -122,6 +123,13 @@ public class CharacterList extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+                Intent intent = new Intent(getApplicationContext(), CharacterDetails.class);
+
+                TextView text = findViewById( R.id.character_list_name);
+                String name = text.getText().toString();
+                intent.putExtra("name", name);
+
+                startActivity(intent);
             }
         });
 
