0a9a602b8f139d3c51de77e6247e708b37f1fb73
naga <CCCDDDBAABDC@naga>
2020-11-05 13:06:41 +0900

    [2020/11/5]@0.1 戻るボタン機能実装

--- NameBattler/app/src/main/java/com/example/namebattler/CharacterOrganization.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterOrganization.java
@@ -3,6 +3,7 @@ package com.example.namebattler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CharacterOrganization extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class CharacterOrganization extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_organization);
+
+
+
+
+
+
+        findViewById(R.id.character_organization_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+
+
+        });
+
     }
 }
