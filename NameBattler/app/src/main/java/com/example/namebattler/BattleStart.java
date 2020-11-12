25786dd66743d922f9b1b739a162ea5a3653699b
naga <CCCDDDBAABDC@naga>
2020-11-12 11:01:51 +0900

    [2020/11/11]@0.8h バトル開始画面レイアウト下書き実装

---
+++ NameBattler/app/src/main/java/com/example/namebattler/BattleStart.java
@@ -0,0 +1,32 @@
+package com.example.namebattler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class BattleStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle_start);
+
+
+        List<Map<String, String>> list = new ArrayList<>();
+//        for(){
+//
+//        }
+
+
+
+
+
+
+
+
+    }
+}
