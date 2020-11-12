8faa9479ee9009a3334b5b532d99dc078085ddcb
naga <CCCDDDBAABDC@naga>
2020-11-12 14:39:44 +0900

    [2020/11/12]@0.1 「キャラクター作成画面」に遷移実装

--- NameBattler/app/src/main/java/com/example/namebattler/CharacterMakeConpletion.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterMakeConpletion.java
@@ -2,7 +2,9 @@ package com.example.namebattler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CharacterMakeConpletion extends AppCompatActivity {
 
@@ -12,6 +14,13 @@ public class CharacterMakeConpletion extends AppCompatActivity {
         setContentView(R.layout.activity_character_make_conpletion);
 
 
+        findViewById(R.id.characterMakeConpletion_continue).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplication(), CharacterMake.class);
+                startActivity(intent);
+            }
+        });
 
 
 
