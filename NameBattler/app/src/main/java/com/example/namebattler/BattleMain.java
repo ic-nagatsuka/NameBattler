9b32db59ef295ffe382f42262b297dd5cf7bc700
naga <CCCDDDBAABDC@naga>
2020-11-24 15:11:14 +0900

    [2020/11/24]@0.5h 各パーティーメンバーのステータス表示機能下書き

--- NameBattler/app/src/main/java/com/example/namebattler/BattleMain.java
+++ NameBattler/app/src/main/java/com/example/namebattler/BattleMain.java
@@ -3,6 +3,15 @@ package com.example.namebattler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import com.name.battler.Player.Party;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BattleMain extends AppCompatActivity {
 
@@ -10,5 +19,30 @@ public class BattleMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_main);
+
+        Party party = null;
+
+        List<Map<String, String>> list = new ArrayList<>();
+//        for(int i = 0; i < party.getmenbers().size(); i++){
+        for(int i = 0; i < 3; i++){
+            Map<String, String> map = new HashMap<>();
+
+//            map.put();
+            list.add(map);
+        }
+
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                list,
+                R.layout.gridview_battle_main,
+                new String[]{},
+                new int[]{}
+        );
+
+        GridView gridView = findViewById(R.id.battle_main_gridView_top);
+        gridView.setAdapter(adapter);
+
+
     }
 }
