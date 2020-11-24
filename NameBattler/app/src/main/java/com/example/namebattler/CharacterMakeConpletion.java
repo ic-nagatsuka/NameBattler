837100272aaa7747ad5495cad25a3afbbdefaa77
naga <CCCDDDBAABDC@naga>
2020-11-24 13:39:11 +0900

    [2020/11/24]@0.5h キャラクター作成完了画面で作成したキャラクターのデータを表示するため、キャラクター作成画面のplayer変数をstaticに変更 レイアウトを調整

--- NameBattler/app/src/main/java/com/example/namebattler/CharacterMakeConpletion.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterMakeConpletion.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.name.battler.Player.Player;
 
 public class CharacterMakeConpletion extends AppCompatActivity {
 
@@ -12,6 +15,35 @@ public class CharacterMakeConpletion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_make_conpletion);
+
+
+        Player player = CharacterMake.player;
+        TextView textView ;
+
+        textView = findViewById(R.id.characterMakeConpletion_name);
+        textView.setText(player.getName());
+
+        textView = findViewById(R.id.characterMakeConpletion_job);
+        textView.setText(player.getJob());
+
+        textView = findViewById(R.id.characterMakeConpletion_set_hp);
+        textView.setText(Integer.toString(player.getHP()));
+
+        textView = findViewById(R.id.characterMakeConpletion_set_mp);
+        textView.setText(Integer.toString(player.getMP()));
+
+        textView = findViewById(R.id.characterMakeConpletion_set_str);
+        textView.setText(Integer.toString(player.getSTR()));
+
+        textView = findViewById(R.id.characterMakeConpletion_set_def);
+        textView.setText(Integer.toString(player.getDEF()));
+
+        textView = findViewById(R.id.characterMakeConpletion_set_agi);
+        textView.setText(Integer.toString(player.getAGI()));
+
+        textView = findViewById(R.id.characterMakeConpletion_set_luck);
+        textView.setText(Integer.toString(player.getLUCK()));
+
 
 
         findViewById(R.id.characterMakeConpletion_continue).setOnClickListener(new View.OnClickListener(){
@@ -29,13 +61,6 @@ public class CharacterMakeConpletion extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-
-
-
-
-
 
     }
 }
