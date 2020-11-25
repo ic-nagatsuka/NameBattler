a71c502f4bcd900f0b7b460e33f9e2351ac3bea8
naga <CCCDDDBAABDC@naga>
2020-11-25 11:29:47 +0900

    [2020/11/25]@0.5h 各パーティーメンバーステータス表示レイアウト実装

--- NameBattler/app/src/main/java/com/example/namebattler/BattleMain.java
+++ NameBattler/app/src/main/java/com/example/namebattler/BattleMain.java
@@ -22,15 +22,21 @@ public class BattleMain extends AppCompatActivity {
 
         Party party = null;
 
+
+
+        makeGridView(R.id.battle_main_gridView_top, party);
+        makeGridView(R.id.battle_main_gridView_bottom, party);
+
+    }
+
+    public void makeGridView(int layout, Party party){
         List<Map<String, String>> list = new ArrayList<>();
-//        for(int i = 0; i < party.getmenbers().size(); i++){
         for(int i = 0; i < 3; i++){
             Map<String, String> map = new HashMap<>();
 
 //            map.put();
             list.add(map);
         }
-
 
         SimpleAdapter adapter = new SimpleAdapter(
                 this,
@@ -40,9 +46,10 @@ public class BattleMain extends AppCompatActivity {
                 new int[]{}
         );
 
-        GridView gridView = findViewById(R.id.battle_main_gridView_top);
+
+        GridView gridView = findViewById(layout);
         gridView.setAdapter(adapter);
-
-
     }
+
+
 }
