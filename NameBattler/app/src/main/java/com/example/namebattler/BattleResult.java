08eff0191ce735a73f3f3671a53fd0333a3700d8
naga <CCCDDDBAABDC@naga>
2020-12-10 13:34:03 +0900

    [2020/12/10]@0.5h バトル結果画面 各パーティーメンバーのステータス表示実装

--- NameBattler/app/src/main/java/com/example/namebattler/BattleResult.java
+++ NameBattler/app/src/main/java/com/example/namebattler/BattleResult.java
@@ -3,6 +3,13 @@ package com.example.namebattler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+import com.name.battler.Player.Party;
+
+import static com.name.battler.GameManager.enemyParty;
+import static com.name.battler.GameManager.myParty;
 
 public class BattleResult extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class BattleResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_result);
+
+        setAdapter(R.id.battleResult_gridView_top, myParty);
+        setAdapter(R.id.battleResult_gridView_bottom, enemyParty);
+
+
     }
+
+    public void setAdapter(int layout, Party party){
+        BaseAdapter adapter = new BaseAdapter_BattleMain(this, party);
+
+        GridView gridView = findViewById(layout);
+        gridView.setAdapter(adapter);
+
+    }
+
 }
