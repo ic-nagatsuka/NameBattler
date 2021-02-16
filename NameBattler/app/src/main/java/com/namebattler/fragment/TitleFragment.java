16e0d603cc2af6e922342d454a643ece29f73990
naga <CCCDDDBAABDC@naga>
2021-02-16 11:54:17 +0900

    [2021/2/16]@0.1h 使用していないoncreateメソッド削除

--- NameBattler/app/src/main/java/com/namebattler/fragment/TitleFragment.java
+++ NameBattler/app/src/main/java/com/namebattler/fragment/TitleFragment.java
@@ -25,15 +25,7 @@ public class TitleFragment extends Fragment {
         fragment.setArguments(bundle);
         return fragment;
     }
-
-
-
-//    @Override
-//    public void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-
-
+    
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
