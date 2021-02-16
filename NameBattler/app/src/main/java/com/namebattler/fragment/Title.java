76d0e29b408eb8a2bbdcc740ac1b8672aa7e9f0a
naga <CCCDDDBAABDC@naga>
2021-02-16 11:36:17 +0900

    [2021/2/16]@0.5h Titleクラスにタイトル表示コード実装

--- NameBattler/app/src/main/java/com/namebattler/fragment/Title.java
+++ NameBattler/app/src/main/java/com/namebattler/fragment/Title.java
@@ -1,6 +1,52 @@
 package com.namebattler.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.namebattler.R;
+
 public class Title extends Fragment {
+
+    static String titleKey = "title";
+
+    public static Fragment newInstans(String title){
+        Title fragment = new Title();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(titleKey, title);
+
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+
+
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_title, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Bundle bnd = getArguments();
+        String title = bnd.getString(titleKey);
+
+        TextView textview = view.findViewById(R.id.fragment_title);
+        textview.setText(title);
+    }
 }
