fbccb60a810ef0844fecc005abe73a7fa474ac39
naga <CCCDDDBAABDC@naga>
2021-02-16 14:21:01 +0900

    [2021/2/16]@0.7h TitleFragmentクラスに「戻る」ボタン追加 「キャラクター一覧画面」レイアウトの「戻る」ボタン削除

--- NameBattler/app/src/main/java/com/namebattler/fragment/TitleFragment.java
+++ NameBattler/app/src/main/java/com/namebattler/fragment/TitleFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,16 +17,17 @@ public class TitleFragment extends Fragment {
 
     static String titleKey = "title";
 
-    public static Fragment newInstans(String title){
+    public static Fragment newInstans(String title, boolean visi){
         TitleFragment fragment = new TitleFragment();
 
         Bundle bundle = new Bundle();
         bundle.putString(titleKey, title);
+        bundle.putBoolean("visi", visi);
 
         fragment.setArguments(bundle);
         return fragment;
     }
-    
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -40,5 +42,17 @@ public class TitleFragment extends Fragment {
 
         TextView textview = view.findViewById(R.id.fragment_title);
         textview.setText(title);
+
+        Button btn = view.findViewById(R.id.fragment_title_back);
+
+        if(getArguments().getBoolean("visi")){
+            btn.setVisibility(View.VISIBLE);
+
+        }else{
+            btn.setVisibility(View.INVISIBLE);
+
+        }
+
+
     }
 }
