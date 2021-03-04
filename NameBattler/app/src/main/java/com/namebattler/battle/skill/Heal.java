6e8c29815516117ad79b35b3911231fcde72b7b3
naga <CCCDDDBAABDC@naga>
2021-03-04 13:31:10 +0900

    [2021/3/4]@0.4h HealクラスにselectTargetメソッドとplayerHpPercentage実装

--- NameBattler/app/src/main/java/com/namebattler/battle/skill/Heal.java
+++ NameBattler/app/src/main/java/com/namebattler/battle/skill/Heal.java
@@ -15,12 +15,29 @@ public class Heal extends SkillBase{
 	
 	@Override
 	public void use(Player attacker, Player target){
+		target = selectTarget(attacker);
 		BattleLog.addLog(attacker.getName() + "は" + this.getName() + "を唱えた！");
-		usePlayerMp(attacker);//mpを消費する
+		usePlayerMp(attacker);
 
 		BattleLog.addLog(target.getName() + "は" + skill.getHealPoint() + "回復した！");
 		//スキル効果
 		target.setHP(Math.min(target.getHP() + skill.getHealPoint(), target.getMaxHp()));
+	}
+
+	public Player selectTarget(Player attacker){
+		Player target = attacker.getParty().getmenbers().get(0);
+
+		for(Player player: attacker.getParty().getmenbers()){
+			if(playerHpPercentage(player) < playerHpPercentage(target)){
+				target = player;
+			}
+		}
+
+		return target;
+	}
+
+	private double playerHpPercentage(Player player){
+		return (double)player.getHP()/(double)player.getMaxHp();
 	}
 
 	@Override
