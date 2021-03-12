0246520883c788c69193d4a27edc94665d4818de
naga <CCCDDDBAABDC@naga>
2021-03-12 11:02:10 +0900

    [2021/3/12]@0.1h コードを整えるためautoformatを使用

--- NameBattler/app/src/main/java/com/namebattler/battle/strategy/AllStrategy.java
+++ NameBattler/app/src/main/java/com/namebattler/battle/strategy/AllStrategy.java
@@ -3,24 +3,24 @@ package com.namebattler.battle.strategy;
 public class AllStrategy {
 
     //コンストラクタ
-    AllStrategy(){
+    AllStrategy() {
 
     }
 
 
     //作戦一覧
-    public enum Strategies{
+    public enum Strategies {
         DAMAGE_PRIORITY(new StrategyOfDamagePriority()),
         LEAST_HP(new StrategyOfLeastHP()),
         NO_SKILL(new StrategyOfNoSkill());
 
         Strategy strategy;
 
-        Strategies(Strategy strategy){
+        Strategies(Strategy strategy) {
             this.strategy = strategy;
         }
 
-        public Strategy getStrategy(){
+        public Strategy getStrategy() {
             return this.strategy;
         }
 
