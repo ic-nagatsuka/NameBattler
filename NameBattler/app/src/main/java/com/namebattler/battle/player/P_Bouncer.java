1db4c26154abeb2d9da28727f6d8cf97ec9326df
naga <CCCDDDBAABDC@naga>
2021-03-12 10:30:14 +0900

    [2021/3/12]@0.4h battleパッケージ内のplayerパッケージにautoformatを使用

--- NameBattler/app/src/main/java/com/namebattler/battle/player/P_Bouncer.java
+++ NameBattler/app/src/main/java/com/namebattler/battle/player/P_Bouncer.java
@@ -4,51 +4,50 @@ import com.namebattler.battle.skill.AllSkill;
 
 import com.namebattler.battle.battlelog.BattleLog;
 
-public class P_Bouncer extends Player{
+public class P_Bouncer extends Player {
 
-	/*=============
-	 * コンストラクタ
-	 =============*/
-	public P_Bouncer(String name)
-	{
-		super(name);
-		this.setJob(AllJob.Job.BOUNCER.getName());
-		this.setCounter(true);
-	}
-	
-	@Override
-	public  void makeCharacter(){
-		this.hp = getNumber(0, 50) + 100;
-		this.mp = getNumber(1, 50) + 60;
-		this.str = getNumber(2, 29) + 1;
-		this.def = getNumber(3, 40) + 50;
-		this.luck = getNumber(4, 50) + 50;
-		this.agi = getNumber(5, 40) + 20;
-	}
-	@Override
-	protected void makeSkill(){
-		useSkill.add(AllSkill.fire);
-		useSkill.add(AllSkill.heel);
-	}
-		
-	@Override
-	public void normalAttack(Player target)
-	{
-		if(rand.nextInt(2) == 0){
-			BattleLog.addLog(this.getName() + "は飛び跳ねた！");
-			super.normalAttack(target);
-		}else{
-			BattleLog.addLog(getName() + "は動かない！");
-		}
-	}
-	
-	@Override
-	protected void counterAttack(Player defender){
-		BattleLog.addLog(this.getName() + "が跳ね返ってきた！");
-		//会心でなければ
-		if( !checkLuckyHit(defender)){
-			this.normalDamage(defender);
-		}
-		this.beforeHp = this.getHP();
-	}
+    /*=============
+     * コンストラクタ
+     =============*/
+    public P_Bouncer(String name) {
+        super(name);
+        this.setJob(AllJob.Job.BOUNCER.getName());
+        this.setCounter(true);
+    }
+
+    @Override
+    public void makeCharacter() {
+        this.hp = getNumber(0, 50) + 100;
+        this.mp = getNumber(1, 50) + 60;
+        this.str = getNumber(2, 29) + 1;
+        this.def = getNumber(3, 40) + 50;
+        this.luck = getNumber(4, 50) + 50;
+        this.agi = getNumber(5, 40) + 20;
+    }
+
+    @Override
+    protected void makeSkill() {
+        useSkill.add(AllSkill.fire);
+        useSkill.add(AllSkill.heel);
+    }
+
+    @Override
+    public void normalAttack(Player target) {
+        if (rand.nextInt(2) == 0) {
+            BattleLog.addLog(this.getName() + "は飛び跳ねた！");
+            super.normalAttack(target);
+        } else {
+            BattleLog.addLog(getName() + "は動かない！");
+        }
+    }
+
+    @Override
+    protected void counterAttack(Player defender) {
+        BattleLog.addLog(this.getName() + "が跳ね返ってきた！");
+        //会心でなければ
+        if (!checkLuckyHit(defender)) {
+            this.normalDamage(defender);
+        }
+        this.beforeHp = this.getHP();
+    }
 }
--- NameBattler/app/src/main/java/com/namebattler/battle/player/Party.java
+++ NameBattler/app/src/main/java/com/namebattler/battle/player/Party.java
@@ -6,56 +6,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Party {
-	/*=============
-	 * フィールド変数
-	 =============*/
-	private List<Player>menbers = new ArrayList<>();
-	
-	/*=============
-	 * コンストラクタ
-	 =============*/
-	private String name;
-	private Strategy strategy;
+    /*=============
+     * フィールド変数
+     =============*/
+    private List<Player> menbers = new ArrayList<>();
 
-	public Party(String name){
-		this.name = name;
-	}
-	
-	/*============
-	 * getメソッド
-	 ============*/
-	public String getName(){
-		return name;
-	}
+    /*=============
+     * コンストラクタ
+     =============*/
+    private String name;
+    private Strategy strategy;
 
-	public Strategy getStrategy(){
-		return strategy;
-	}
-	
-	public List<Player> getmenbers(){
-		return menbers;
-	}
-	/*============
-	 * setメソッド
-	 ============*/
-	public void setStrategy(Strategy strategy){
-		this.strategy = strategy;
-	}
+    public Party(String name) {
+        this.name = name;
+    }
+
+    /*============
+     * getメソッド
+     ============*/
+    public String getName() {
+        return name;
+    }
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public List<Player> getmenbers() {
+        return menbers;
+    }
+
+    /*============
+     * setメソッド
+     ============*/
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
 
 
+    /*
+     * メソッド
+     */
+    public void appendPlayer(Player player) {
+        this.menbers.add(player);
+    }
 
-	/*
-	 * メソッド
-	 */
-	public void appendPlayer(Player player)
-	{
-		this.menbers.add(player);
-	}
-	
-	public void removePlayer(Player player)
-	{
-		this.menbers.remove(player);
-	}
+    public void removePlayer(Player player) {
+        this.menbers.remove(player);
+    }
 
-	
+
 }
