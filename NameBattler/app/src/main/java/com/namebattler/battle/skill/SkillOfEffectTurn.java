3b6ccaab2657590b688c435cf6ac6501ec35776c
naga <CCCDDDBAABDC@naga>
2021-03-12 11:01:21 +0900

    [2021/3/12]@0.2h コードを整えるためautoformatを使用

--- NameBattler/app/src/main/java/com/namebattler/battle/skill/SkillOfEffectTurn.java
+++ NameBattler/app/src/main/java/com/namebattler/battle/skill/SkillOfEffectTurn.java
@@ -2,73 +2,78 @@ package com.namebattler.battle.skill;
 
 import com.namebattler.battle.player.Player;
 
-public class SkillOfEffectTurn extends Skill{
+public class SkillOfEffectTurn extends Skill {
 
-	public static class StateEffect{
-		/*=============
-		 * フィールド変数
-		 =============*/
-		SkillOfEffectTurn skill;
-		int turn;
-		String stateChar;
-		/*=============
-		 * コンストラクタ
-		 =============*/	
-		StateEffect(SkillOfEffectTurn skill, int turn, String stateChar){
-			this.skill = skill;
-			this.turn = turn;
-			this.stateChar = stateChar;
-		}
-		/*============
-		 * Get
-		 ============*/
-		public SkillOfEffectTurn getSkill(){
-			return this.skill;
-		}
-		
-		public int getTurn(){
-			return this.turn;
-		}
+    public static class StateEffect {
+        /*=============
+         * フィールド変数
+         =============*/
+        SkillOfEffectTurn skill;
+        int turn;
+        String stateChar;
 
-		public String getStateChar(){
-			return this.stateChar;
-		}
-		/*============
-		 * Set
-		 ============*/
-		public void setTurn(int turn){
-			this.turn = turn;
-		}
-		
-	}
-	/*================
-	 * フィールド変数
-	 ================*/
-	int effectTurn;
-	
-	//===========
-	//コンストラクタ
-	//===========
-	SkillOfEffectTurn(SkillType type, String name, int useMp, int effectTurn){
-		super(type, name, useMp);
-		this.effectTurn = effectTurn;
-	}
+        /*=============
+         * コンストラクタ
+         =============*/
+        StateEffect(SkillOfEffectTurn skill, int turn, String stateChar) {
+            this.skill = skill;
+            this.turn = turn;
+            this.stateChar = stateChar;
+        }
 
-	/*============
-	 * Getメソッド
-	 ============*/
-	public int getEffectTurn(){
-		return this.effectTurn;
-	}
-	
-	/**
-	 * 状態異常効果
-	 * @param target 行動プレイヤー
-	 */
-	public void effect(Player target, int turn){
-		//オーバーライド
-	}
-	
+        /*============
+         * Get
+         ============*/
+        public SkillOfEffectTurn getSkill() {
+            return this.skill;
+        }
+
+        public int getTurn() {
+            return this.turn;
+        }
+
+        public String getStateChar() {
+            return this.stateChar;
+        }
+
+        /*============
+         * Set
+         ============*/
+        public void setTurn(int turn) {
+            this.turn = turn;
+        }
+
+    }
+
+    /*================
+     * フィールド変数
+     ================*/
+    int effectTurn;
+
+    //===========
+    //コンストラクタ
+    //===========
+    SkillOfEffectTurn(SkillType type, String name, int useMp, int effectTurn) {
+        super(type, name, useMp);
+        this.effectTurn = effectTurn;
+    }
+
+    /*============
+     * Getメソッド
+     ============*/
+    public int getEffectTurn() {
+        return this.effectTurn;
+    }
+
+    /**
+     * 状態異常効果
+     *
+     * @param target 行動プレイヤー
+     */
+    public void effect(Player target, int turn) {
+        //オーバーライド
+    }
+
 }
 
 
--- NameBattler/app/src/main/java/com/namebattler/battle/skill/SkillOfHeel.java
+++ NameBattler/app/src/main/java/com/namebattler/battle/skill/SkillOfHeel.java
@@ -1,25 +1,25 @@
 package com.namebattler.battle.skill;
 
 
-public class SkillOfHeel extends Skill{
-	/*=============
-	 * フィールド変数
-	 =============*/
-	int heelPoint;	//回復力
-	
-	/*=============
-	 * コンストラクタ
-	 =============*/
-	SkillOfHeel(SkillType type, String name, int useMp, int heelPoint){
-		super(type, name, useMp);
-		this.heelPoint = heelPoint;
-	}
-	
-	/*============
-	 *Get 
-	 ============*/
-	public int getHeelPoint(){
-		return this.heelPoint;
-	}
-	
+public class SkillOfHeel extends Skill {
+    /*=============
+     * フィールド変数
+     =============*/
+    int heelPoint;    //回復力
+
+    /*=============
+     * コンストラクタ
+     =============*/
+    SkillOfHeel(SkillType type, String name, int useMp, int heelPoint) {
+        super(type, name, useMp);
+        this.heelPoint = heelPoint;
+    }
+
+    /*============
+     *Get
+     ============*/
+    public int getHeelPoint() {
+        return this.heelPoint;
+    }
+
 }
