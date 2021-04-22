e75bc024ac32845e74edf42c8723411a27b1b3bb
naga <CCCDDDBAABDC@naga>
2021-04-22 10:15:04 +0900

    [2021/4/22]@0.3h <Brance>作戦クラスの名前を変更 処理を実装

---
+++ NameBattler/app/src/main/java/com/namebattler/battle/strategy/PleaseFree.java
@@ -0,0 +1,26 @@
+package com.namebattler.battle.strategy;
+
+import com.namebattler.battle.party.Party;
+import com.namebattler.battle.player.Player;
+import com.namebattler.battle.skill.SkillBase;
+
+public class PleaseFree extends Strategy {
+
+    /*=============
+     * コンストラクタ
+     =============*/
+    public PleaseFree() {
+        this.name = "自由にどうぞ";
+    }
+
+    @Override
+    public void action(Player attacker, Party defenceParty) {
+        Player target = randomSelectDefender(defenceParty);
+        if(attacker.canSkill()){
+            SkillBase skill = attacker.getNowUseSkillOnly().get(
+                    rand.nextInt(attacker.getNowUseSkillOnly().size()));
+            attacker.useSkill(skill, target);
+        }
+    }
+
+}
